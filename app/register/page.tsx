9652a9789fc9724707dba5cd0b78a1a31a6b3251
Kirill Kravtsov <nvarscar@gmail.com>
2026-06-05 14:31:31 -0700

    make register page dynamic to load turnstile

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,7 @@
 import RegisterForm from "./RegisterForm";
 
+export const dynamic = "force-dynamic";
+
 export default function RegisterPage() {
   const siteKey = process.env.TURNSTILE_SITE_KEY ?? "1x00000000000000000000AA";
   return <RegisterForm siteKey={siteKey} />;
