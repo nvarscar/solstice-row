47164dde02559ddf106150185b925938bea52790
Kirill Kravtsov <nvarscar@gmail.com>
2026-06-05 00:02:58 -0700

    fixes for scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <head>
         <script
           src="https://identity.netlify.com/v1/netlify-identity-widget.js"
